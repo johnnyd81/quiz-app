8bd35db276cf546a1dee772ffb99825ce1fd651c
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2023-10-07 11:59:36 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const correctAnswers = ["C", "B", "B", "A", "C"];
+const correctAnswers = ["C", "B", "B", "A", "C", "B", "C", "A", "B", "B"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 
@@ -6,35 +6,41 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let score = 0;
+  //an array holds all the responses from the user
+  //each question has three choices that has the same name attribute. This ensures that only one option can be chosen
   let userAnswers = [
     form.q1.value,
     form.q2.value,
     form.q3.value,
     form.q4.value,
     form.q5.value,
+    form.q6.value,
+    form.q7.value,
+    form.q8.value,
+    form.q9.value,
+    form.q10.value,
   ];
 
-  //check if the user answers are correct
-  userAnswers.forEach((answer, index) => {
-    if (answer === correctAnswers[index]) {
-      score += 20;
+  //loop over the userAnswers to see if they match the correctAnswers
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer == correctAnswers[index]) {
+      score += 10;
     }
   });
 
-  //show result at the top of the page
-  //by using scrollTo(0, 0) the x and the y axis are 0 meaning the position is right at the top of the page
-  scrollTo(0, 0); //represents the x and y axis of the page
+  //display the result at the top of the page
+  scroll(0, 0); //the arguments represent the x and y axis
   result.querySelector("span").textContent = `${score}%`;
   result.classList.remove("d-none");
 
-  //animate the score
-  let output = 0;
+  //animate the result
+  let scoreOutput = 0;
   const timer = setInterval(() => {
-    result.querySelector("span").textContent = `${output}%`;
-    if (score === output) {
+    result.querySelector("span").textContent = `${scoreOutput}%`;
+    if (score == scoreOutput) {
       clearInterval(timer);
     } else {
-      output++;
+      scoreOutput++;
     }
   }, 10);
 });
